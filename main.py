e66c7dad0fda0829d361ab5403e4afc29cdbcdae
Matthew Pedone <12798189+mpedone@users.noreply.github.com>
2025-04-04 12:44:53 -0400

    add clock and delta time to limit to 60 FPS
    I was a little confused by the instruction to call Clock.tick(60), then
    divide it by 1000 and assign this value to dt. The .tick() method
    pauses execution, but we save it to a variable; I think it's a method
    that makes the change and also returns a value, like List.pop().

--- main.py
+++ main.py
@@ -6,16 +6,25 @@ from constants import *
 
 def main():
     pygame.init()
+    clock = pygame.time.Clock()
+    dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
+    
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+
+        # Fill the screen with black and refresh
         screen.fill((0,0,0))
         pygame.display.flip()
+
+        #limit the frame rate to 60 FPS
+        dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
     main()
