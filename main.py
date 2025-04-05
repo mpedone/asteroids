b3bc7f3ca5ebc452facbce5260fd40c647e26b18
Matthew Pedone <12798189+mpedone@users.noreply.github.com>
2025-04-05 13:10:04 -0400

    create CircleShape and Player classes
    CircleShape will be our asteroids, the Player class inherits from
    CircleShape, which will give us a circular hitbox. Player is now static
    in the middle of the screen.

--- main.py
+++ main.py
@@ -2,13 +2,15 @@
 # the open-source pygame library
 # throughout this file
 import pygame
-from constants import *
+from player import Player
+from constants import SCREEN_WIDTH, SCREEN_HEIGHT
 
 def main():
     pygame.init()
     clock = pygame.time.Clock()
     dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    player = Player(x = SCREEN_WIDTH / 2, y = SCREEN_HEIGHT / 2)
 
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
@@ -21,6 +23,7 @@ def main():
 
         # Fill the screen with black and refresh
         screen.fill((0,0,0))
+        player.draw(screen)
         pygame.display.flip()
 
         #limit the frame rate to 60 FPS
