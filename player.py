8fcf639c5b5dcc534c377accb5ad402cb2b5a604
Matthew Pedone <12798189+mpedone@users.noreply.github.com>
2025-04-07 21:48:17 -0400

    add new constant to control shoot speed

--- player.py
+++ player.py
@@ -4,12 +4,14 @@ from shot import Shot
 from constants import (PLAYER_RADIUS, 
                        PLAYER_TURN_SPEED, 
                        PLAYER_SPEED, 
-                       PLAYER_SHOOT_SPEED)
+                       PLAYER_SHOOT_SPEED, 
+                       PLAYER_SHOOT_COOLDOWN)
 
 class Player(CircleShape):
     def __init__(self, x, y):
         super().__init__(x, y, PLAYER_RADIUS)
         self.rotation = 0
+        self.shoot_timer = 0
 
     def triangle(self):
         forward = pygame.Vector2(0, 1).rotate(self.rotation)
@@ -30,10 +32,14 @@ class Player(CircleShape):
         self.position += forward * PLAYER_SPEED * dt
 
     def shoot(self):
+        if self.shoot_timer > 0:
+            return
+        self.shoot_timer = PLAYER_SHOOT_COOLDOWN
         shot = Shot(self.position[0], self.position[1])
         shot.velocity = pygame.Vector2(0,1).rotate(self.rotation) * PLAYER_SHOOT_SPEED
 
     def update(self, dt):
+        self.shoot_timer -= dt
         keys = pygame.key.get_pressed()
 
         if keys[pygame.K_LEFT]:
